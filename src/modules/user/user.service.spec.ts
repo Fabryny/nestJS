941988e3f851b14299ea1fe4c6ab91bfce169de7
Matheus <mfabryny@minsait.com>
2022-10-07 18:49:15 -0300

    Tests created

--- src/modules/user/user.service.spec.ts
+++ src/modules/user/user.service.spec.ts
@@ -1,74 +1,153 @@
-import { BadRequestException, InternalServerErrorException } from '@nestjs/common';
+
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UserDto } from './dto/user-dto';
-import { User } from './entities/user.entity';
-import { UserRepository } from './user.repository';
 import { UserService } from './user.service';
+import { Repository } from 'typeorm';
+import { randomUUID } from 'crypto';
+import { User } from './entities/user.entity';
+import { mock } from 'jest-mock-extended';
 
-describe('UserService', () => {
+describe(UserService, () => {
   let service: UserService;
-  let repository: UserRepository;
-  
-  const DefaultUserDto: User = {
-    firstName: "default",
-    lastName: "default",
-    username: "default",
-    email: "default",
-    password: "default",
-    active: true,
-    id: 'id',
-    salt: 'salt'
-
-  }
+  let repository: Repository<User>;
+  const wrongId = 'wrong_id';
+  let DefaultUser: User;
+  let DefaultUserDto: CreateUserDto;
 
   beforeEach(async () => {
+    const mockRepository = mock<Repository<User>>();
+    const RepositoryToken = getRepositoryToken(User);
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService, UserRepository],
+      providers: [
+        {
+          provide: RepositoryToken,
+          useValue: mockRepository,
+        },
+        UserService,
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
-    repository = module.get<UserRepository>(UserRepository);
+    repository = module.get<Repository<User>>(RepositoryToken);
+
+    DefaultUser = {
+      id: 'randomId',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      username: 'john',
+      password: 'Test1234',
+      firstName: 'John',
+      lastName: 'Doe',
+      email: `${randomUUID()}@dispostable.com`,
+      salt: 'saltHash',
+      active: true,
+    };
+
+    DefaultUserDto = {
+      ...DefaultUser,
+    };
+
+    jest.spyOn(repository, 'create').mockReturnValue(DefaultUser);
+    jest.spyOn(repository, 'save').mockResolvedValue(DefaultUser);
+    jest.spyOn(repository, 'find').mockResolvedValue([DefaultUser]);
+    jest.spyOn(repository, 'findOneBy').mockResolvedValue(DefaultUser);
+    jest.spyOn(repository, 'remove');
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
-    expect(repository).toBeDefined();
   });
 
-
   describe(UserService.prototype.create, () => {
+    it('should success', async () => {
+      const user = await service.create(DefaultUserDto);
 
-    it('should be success', () => {
-      jest.spyOn(repository, 'create').mockReturnValue(DefaultUserDto);
-      const user = service.create(DefaultUserDto);
-      expect(user.username).toBe(DefaultUserDto.username);
       expect(user.id).toBeDefined();
+      expect(user.firstName).toBe(DefaultUserDto.firstName);
     });
-
-    it('should be fail', () => {
-      jest.spyOn(repository, 'create').mockImplementationOnce(() => {
-        throw new InternalServerErrorException();
-      });
-      const t = () => service.create(DefaultUserDto);
-      expect(t).toThrow(BadRequestException);
-    });
-    
   });
 
   describe(UserService.prototype.update, () => {
-    it('should be defined', () => {  
-      jest.spyOn(repository, 'update').mockReturnValue(DefaultUserDto);
-      const user = service.update('1', DefaultUserDto);
-      expect(user.username).toBe(DefaultUserDto.username);
+    it('should success', async () => {
+      let user = await service.create(DefaultUserDto);
+      const newFirstName = 'myNewName';
+      user = await service.update(user.id, {
+        firstName: newFirstName,
+      });
+
+      expect(user.id).toBeDefined();
+      expect(user.lastName).toBe(DefaultUserDto.lastName);
+      expect(user.firstName).toBe(newFirstName);
+    });
+    it('should success', async () => {
+      let user = await service.create(DefaultUserDto);
+      const newFirstName = 'myNewName';
+      user = await service.update(user.id, {
+        lastName: newFirstName,
+      });
+
+      expect(user.id).toBeDefined();
+      expect(user.firstName).toBe(DefaultUserDto.firstName);
+      expect(user.lastName).toBe(newFirstName);
     });
   });
-/* 
-  describe(UserService.prototype.findAll, () => {
-    it('should be defined', () => {
-      expect(service).toBeDefined();
-      expect(repository).toBeDefined();
-    });
-  }); */
 
+  describe(UserService.prototype.findAll, () => {
+    it('should be success with default', async () => {
+      const users = await service.findAll();
+      expect(users.length).toBe(1);
+    });
+
+    it('should be success with empty return', async () => {
+      jest.spyOn(repository, 'find').mockResolvedValue([]);
+      const users = await service.findAll();
+      expect(users.length).toBe(0);
+    });
+
+    it('should be success with 2 users', async () => {
+      jest
+        .spyOn(repository, 'find')
+        .mockResolvedValue([DefaultUser, DefaultUser]);
+      const users = await service.findAll();
+      expect(users.length).toBe(2);
+    });
+  });
+
+  describe(UserService.prototype.findOne, () => {
+    it('should success', async () => {
+      const user = await service.create(DefaultUserDto);
+      const userFound = await service.findOne(user.id);
+      expect(userFound.id).toBe(user.id);
+    });
+
+    it('should fail', () => {
+      jest
+        .spyOn(repository, 'findOneBy')
+        .mockImplementationOnce(async () => null);
+      const t = async () => {
+        await service.findOne(wrongId);
+      };
+      expect(t).rejects.toThrow(NotFoundException);
+    });
+  });
+
+  describe(UserService.prototype.remove, () => {
+    it('should Success', async () => {
+      const id = randomUUID();
+      service.remove(id);
+    });
+
+    it('should Throw', () => {
+      jest
+        .spyOn(repository, 'findOneBy')
+        .mockImplementationOnce(async () => null);
+
+      const toThrow = async () => {
+        await service.remove(wrongId);
+      };
+      expect(toThrow).rejects.toThrow(NotFoundException);
+    });
+  });
 });
